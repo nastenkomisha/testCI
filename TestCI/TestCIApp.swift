cfcb4f2dee23599d82099efa44918d0d7b39a9a9
nastenkomisha <nastenkomisha@gmail.com>
2023-01-11 10:27:57 +0400

    Test CI

---
+++ TestCI/TestCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCIApp.swift
+//  TestCI
+//
+//  Created by Mikhail Nastenko on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
